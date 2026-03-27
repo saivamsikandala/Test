9710ead0429c9b743c581cd410240cd8b50eb9ff
saivamsikandala <kandalasaivamsi@gmail.com>
2026-03-27 13:07:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple alert for form (placeholder)
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! We will contact you soon.");
+});
